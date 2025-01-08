af782433e20aeb67ae97d3aba9086728f097fe3b
ahsen17 <ahsenedward@qq.com>
2025-01-09 02:17:07 +0800

    refactor: base resource view refine

--- common/baseView.py
+++ common/baseView.py
@@ -37,7 +37,7 @@ class ResourceViewMgr(View):
         }
 
         self._result = {}
-        self._rawFlag = False
+        self._rawResp = False
 
         super(ResourceViewMgr, self).__init__(*args, **kwargs)
 
@@ -47,15 +47,16 @@ class ResourceViewMgr(View):
         self._META["METHOD"] = request.method
         self._META["QUERY_STRING"] = request.path
 
-        return self._requestDistributes(*args, **kwargs)
+        self._requestDistributes(*args, **kwargs)
+        return self._result if self._rawResp else _response(**self._result)
     
     def resp(self, code=1001, status=False, data=None, message=""):
-        self._result.update({k: v for k, v in locals().items() if k!= "self"})
+        self._result.update({k: v for k, v in locals().items() if not k.startswith("self")})
 
     def respRaw(self, data: HttpResponse):
-        assert isinstance(data, HttpResponse), TypeError("data must be an instance of HttpResponse.")
+        assert isinstance(data, HttpResponse), TypeError("Data must be an instance of HttpResponse.")
         self._result = data
-        self._rawFlag = True
+        self._rawResp = True
     
     def _requestDistributes(self, *args, **kwargs):
         """
@@ -63,26 +64,19 @@ class ResourceViewMgr(View):
         """
         reqMethod = self._META["METHOD"]
         if reqMethod not in self._PERMITTED_METHODS:
-            self.resp(code=CODE.METHOD_NOT_ALLOWED, message="Method not allowed.")
-            return _response(**self._result)
+            return self.resp(code=CODE.METHOD_NOT_ALLOWED, message="Method not allowed.")
 
         qStrs = [part for part in self._META['QUERY_STRING'].split('/') if part]
-        if qStrs[0] != "api" or len(qStrs) < 4:
-            self.resp(code=CODE.BAD_REQUEST)
-            return _response(**self._result)
+        if qStrs[0] != "api" or len(qStrs) < 4 or qStrs[1] != self._VERSION:
+            raise NotImplementedError("Invalid request path.")
         
         version = qStrs[1]
-        if version != self._VERSION:
-            self.resp(code=CODE.BAD_REQUEST, message=f"Version [{version}] not supported.")
-            return _response(**self._result)
-
         resource = qStrs[2]
         operate = qStrs[3]
         itemId = qStrs[4] if len(qStrs) > 4 else None
 
         if not hasattr(self, operate):
-            self.resp(code=CODE.NOT_FOUND, message=f"[{operate}] not supported.")
-            return _response(**self._result)
+            return self.resp(code=CODE.NOT_FOUND, message=f"Operation [{operate}] not supported.")
         
         try:
             _req = self._META['HTTP_REQUEST']
@@ -90,7 +84,6 @@ class ResourceViewMgr(View):
             getattr(self, operate)(resource, itemId, *args, **kwargs)
         except Exception as e:
             self.resp(code=CODE.INTERNAL_SERVER_ERROR, message=str(e))
-        return _response(**self._result) if not self._rawFlag else self._result
 
         
 class ResourceViewMgrV2(ResourceViewMgr):
